ffad67837aa2fb067111c190422046cb81f63bcd
GislaineJessica <GislainehJessica@gmail.com>
2019-11-29 11:56:19 -0300

    Cadastro de Estudantes

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,8 +1,40 @@
+import * as Yup from 'yup'
 import Student from '../models/Student'
+import User from '../models/User'
 
 class StudentController {
 	async store(req, res) {
 		const { name, email, idade, peso, altura } = req.body
+		const id = req.user_id
+
+		// Verificar se é um admnistrador que está logado
+		const user = await User.findByPk(id)
+		if (!user.admim) {
+			return res.status(401).json({
+				message: 'Usuario não é administrador, cadastro não pode ser realizado',
+			})
+		}
+
+		// Validação das entradas
+		const schema = await Yup.object().shape({
+			name: Yup.string().required(),
+			email: Yup.string()
+				.email()
+				.required(),
+			idade: Yup.number(),
+			peso: Yup.number(),
+			altura: Yup.number(),
+		})
+
+		if (!schema.isValid(req.body)) {
+			return res.status(400).json({ message: 'Falha na validação de dados' })
+		}
+
+		// Verificar se usuario existe
+		const userExists = await User.findOne({ where: { email: req.body.email } })
+		if (userExists) {
+			return res.status(400).json({ error: 'email já está cadastrado' })
+		}
 
 		const newStudent = {
 			name,
@@ -13,7 +45,6 @@ class StudentController {
 		}
 
 		await Student.create(newStudent)
-
 		return res.json(newStudent)
 	}
 
@@ -24,9 +55,46 @@ class StudentController {
 
 	async update(req, res) {
 		const { name, email, idade, peso, altura } = req.body
-		const { id } = req.params.id
+		const { id } = req.params
+		const admim_id = req.user_id
+
+		// Verificar se é um admnistrador que está logado
+		const user = await User.findByPk(admim_id)
+		if (!user.admim) {
+			return res.status(401).json({
+				message: 'Usuario não é administrador, cadastro não pode ser realizado',
+			})
+		}
+
+		// Validação das entradas
+		const schema = await Yup.object().shape({
+			name: Yup.string(),
+			email: Yup.string()
+				.email()
+				.required(),
+			idade: Yup.number(),
+			peso: Yup.number(),
+			altura: Yup.number(),
+		})
+
+		if (!(await schema.isValid(req.body))) {
+			return res.status(400).json({ message: 'Falha na validação de dados' })
+		}
+
+		// Validação das mudanças
+		const student = await Student.findByPk(id)
+		console.log(student.email)
+
+		// Verificar se email passado é diferente(quer trocar)
+		if (email !== student.email) {
+			// verificar se email novo já não existe na base de dados
+			if (email === Student.findOne({ where: { email } })) {
+				return res.status(400).json({ error: 'email já está cadastrado' })
+			}
+		}
 
 		const newStudent = {
+			id: student.id,
 			name,
 			email,
 			idade,
@@ -34,7 +102,7 @@ class StudentController {
 			altura,
 		}
 
-		await Student.update()
+		await Student.update(newStudent, { where: { id } })
 
 		return res.json(newStudent)
 	}
