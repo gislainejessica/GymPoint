65efcae5a6b3c1aaca8cdd3e58fce1a5ac6acee0
GislaineJessica <GislainehJessica@gmail.com>
2019-12-01 14:43:58 -0300

    Web first page

---
+++ web/src/components/Header/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #fff;
+  padding: 0 30px;
+`;
+export const Content = styled.div`
+  height: 64px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #eee;
+    }
+  }
+  nav {
+    ul {
+    display: flex;
+    flex-direction: row;
+    }
+  }
+`;
